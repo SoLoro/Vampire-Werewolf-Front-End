7530cba7db8aba2e7a4551ee4602ed1183edd0f3
Vyacheslav <ggg9gg6@yandex.ru>
2021-08-25 23:24:35 +0300

    JS script animation button + section switching

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+button1.onmouseover = function(){inf.style.background='url(img/people-min.jpg) center center/cover no-repeat';
+title2.style.display='none';
+subtitle2.style.display='none';
+title1.style.display='block';
+subtitle1.style.display='block'
+};
+
+button2.onmouseover = function(){inf.style.background='url(img/wolf-min.jpg) center center/cover no-repeat';
+title1.style.display='none';
+subtitle1.style.display='none';
+title2.style.display='block';
+subtitle2.style.display='block'
+};
+
